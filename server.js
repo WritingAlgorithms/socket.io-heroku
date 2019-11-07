c6924ec30e17416e9e904bf0630a3147b6efc68c
WritingAlgorithms <charles.andoni@hotmail.com>
2019-11-06 23:03:40 -0600

    first commit

--- server.js
+++ server.js
@@ -12,10 +12,56 @@ const server = express()
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
+//const nsp_lobby = io.of('lobby');
 
-io.on('connection', (socket) => {
+var players = {};
+
+
+io.on('connection', function(socket) {
   console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+
+  socket.on('setNickname', function(nicknameData) {
+    players[socket.id] = {};
+    players[socket.id].nickname = nicknameData.nickname;
+    console.log(players);
+    console.log(nicknameData.nickname + " Joined the Lobby");
+    socket.join('lobby');
+    io.to('lobby').emit('getPlayers', players);
+    //io.in('lobby').emit('getPlayers', players);
+  });
+
+  socket.on('sendMsg', function(msgData) {
+    let prepmsg = players[socket.id].nickname + ": " + msgData.message;
+    console.log(prepmsg);
+
+    io.to('lobby').emit('newMsg', { message: prepmsg });
+    //io.in('lobby').emit('newMsg', { message: prepmsg });
+  });
+
+  socket.on('disconnect', () => {
+     console.log('Client disconnected');
+     delete players[socket.id];
+     io.to('lobby').emit('getPlayers', players);
+    //io.in('lobby').emit('getPlayers', players);
+  });
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+/*nsp_lobby.on('connection', (socket) => {
+  console.log('Client connected');
+  players[socket.id] = {};
+
+  socket.on('setNickname', function(nicknameData) {
+    players[socket.id].nickname = nicknameData.nickname;
+    console.log(players);
+    socket.broadcast.emit('getPlayers', players);
+    socket.emit('getPlayers', players);
+  });
+
+
+  socket.on('disconnect', () => {
+     console.log('Client disconnected');
+     delete players[socket.id];
+  });
+
+});*/
